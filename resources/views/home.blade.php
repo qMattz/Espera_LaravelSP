d31bcf36c01b84cf558be884f91602246defc12b
iMatt <esperamattm@gmail.com>
2026-02-10 13:27:46 +0800

    Create home.blade.php for student portal homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('content')
+    <h1>
+        Welcome to the Student Portal
+    </h1>
+    <p>
+        This is the home page of the Student Portal.
+        Use the navigation buttons to explore the student list and manage student records.
+    </p>
+    <a href="/students" class="button">Go to Student List</a>
+@endsection
