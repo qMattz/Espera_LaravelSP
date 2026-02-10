2177a34c548681929d36d33d1806a9063ace92e5
iMatt <esperamattm@gmail.com>
2026-02-10 13:27:20 +0800

    Add student profile view template

---
+++ resources/views/students/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('content')  
+    <title>Student Profile</title>
+   
+        <h1>Student Profile</h1>
+        <p><strong>Name:</strong> Matt Espera</p>
+        <p><strong>Email:</strong> matt.espera@email.com</p>
+        <p><strong>Course:</strong> BS Information Technology</p>
+        <p><strong>Year Level:</strong> 2nd Year</p>
+
+        <a href="/students" class="button">Back to Student List</a>
+@endsection
